64ac68bd08aedf985989ca6764e0c96ef25b89e2
Cristian Puerto <capuerto@correo.udistrital.edu.co>
2022-09-23 16:53:46 -0500

    v 0.02

--- jugador.py
+++ jugador.py
@@ -1,8 +1,21 @@
+from ficha import Ficha
+
+
+
 class Jugador:
 
     def __init__(self):
-
+        self.miFicha=Ficha()
         pass
 
-    def realizar_jugada():
-        }
+    def realizar_jugada(self,unTablero):
+
+        while x>2 and x<0:
+            x=int(input("Ingresa la fila"))
+        y=-1
+        while x>2 and x<0:
+            y=int(input("Ingresa la columna"))
+
+        unTablero[x][y]= self.miFicha.simbolo
+
+        return unTablero
